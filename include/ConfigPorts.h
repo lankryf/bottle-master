6e42574246d5a877fb41b9289dc64dd999ecdb8a
lankryf <lankrf@gmail.com>
2023-04-27 12:54:29 +0300

    comment

--- include/ConfigPorts.h
+++ include/ConfigPorts.h
@@ -5,11 +5,16 @@
 #define CLK 18
 #define DT 19
 #define SW 21
+
+//termo
 #define thermoDO 33
 #define thermoCS 25
 #define thermoCLK 26
+
 #define stepperSTEP 12
 #define stepperDIR 14
+
 #define TCO 32 // ThermoControlOut 
+
 // indicator
 #define workIndicator 5
